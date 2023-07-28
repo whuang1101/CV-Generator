c677588f43174b8072b24e34799b6bb665d2f92f
Wilson Huang <wilsonhuang11012000@gmail.com>
2023-07-28 13:14:54 -0400

    Add education frame still need to implement logic

--- src/layout.jsx
+++ src/layout.jsx
@@ -3,11 +3,24 @@ import { dropDownIcon , personalItem, educationIcon, workIcon,  emailIcon, phone
 
 export default function Layout () {
     const [personal, setPersonal] = useState({detail: "person-details", display:"hide"});
-
+    const [personalEducation, setPersonalEducation] = useState({detail:"education", display: "hide"});
+    const [educationDetails, setEducationDetails] = useState({click:false, format:"education-click"});
+    const [dropdown, setDropDown] = useState("hello")
+    const [educationDrop, setEducationDrop] = useState("hello")
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [phone, setPhone] = useState("");
     const [address, setAddress] = useState("");
+    const handleEducationDetails = () => {
+        if(!educationDetails.click){
+            const newEducationDetails = {... educationDetails, click:true, format:"education-details"}
+            setEducationDetails(newEducationDetails);
+        }
+        else{
+            const newEducationDetails = {... educationDetails, click:false, format:"education-click"}
+            setEducationDetails(newEducationDetails);
+        }
+    }
     const handleName = (e) => {
         setName(e.target.value);
     }
@@ -22,14 +35,29 @@ export default function Layout () {
     }
 
 
+
+    const handleEducationClick = () => {
+        if (personalEducation.display === "hide") {
+            const newEducation = {... personalEducation, detail: "education-drop", display: "show"};
+            setPersonalEducation(newEducation)
+            setEducationDrop("dropdown-rotate")
+        }
+        else {
+            const newEducation = {... personalEducation, detail: "education", display: "hide"};
+            setPersonalEducation(newEducation)
+            setEducationDrop("dropdown")
+        }
+    }
     const handlePersonalClick = () => {
         if(personal.display === "hide"){
             const newPersonal = { ...personal, detail: "person-details-drop", display: "show"}
+            setDropDown("dropdown-rotate")
             setPersonal(newPersonal)
         }
         else {
             const newPersonal = { ...personal, detail: "person-details", display:"hide"}
             setPersonal(newPersonal)
+            setDropDown("dropdown")
         }
     }
 
@@ -41,14 +69,14 @@ export default function Layout () {
             <div className="tools">
             
             </div>
-            <div className={personal.detail}>
+            <div className={personal.detail} onClick={handlePersonalClick}> 
                 <div className="title">
                     <div className="icon">
                         {personalItem}
                     </div>
                     <div className="titleName">Personal Details</div>
                 </div>
-                <div className="dropdown" onClick={handlePersonalClick}>
+                <div className={dropdown}>
                     {dropDownIcon}
                 </div>
             </div>
@@ -61,7 +89,7 @@ export default function Layout () {
                 </div>
                 <div className="email">
                     <div className="personal-title">
-                        Email
+                        Email:
                     </div>
                     <input type="text" placeholder="Enter email" onChange={handleEmail}/>
                 </div>
@@ -73,19 +101,55 @@ export default function Layout () {
                 </div>
                 <div className="address" onChange={handleAddress}>
                     <div className="personal-title">
-                        Address
+                        Address:
                     </div>
                     <input type="text" placeholder="City, Country" />
                 </div>
             </div>
-            <div className="education">
+            <div className= {personalEducation.detail} onClick={handleEducationClick}>
                 <div className="title">
                     <div className="icon">
                             {educationIcon}
                         </div>
-                    <div className="titleName">Education</div>
+                    <div className="titleName">Education</div> 
                 </div>
-                {dropDownIcon}
+                <div className={educationDrop}>
+                    {dropDownIcon}
+                </div>
+            </div>
+            <div className={[personalEducation.display, educationDetails.format].join(" ")}>
+                {!educationDetails.click ?
+                <button className = "add-education" onClick={handleEducationDetails}>
+                    + Education
+                </button>: <> 
+                <div className="school">
+                    <div className="school-title">
+                        School:
+                    </div>
+                    <input type="text" placeholder="Enter school/university"/>
+                </div>
+                <div className="degree">
+                    <div className="degree-title">
+                        Degree:
+                    </div>
+                    <input type="text" placeholder="Enter Degree/ Field Of Study"/>
+                </div>
+                <div className="dates">
+                    <div className="start-date">
+                        <div className="start-title">
+                            Start Date
+                        </div>
+                        <input type="text" placeholder="Start Date"/>
+                    </div>
+                    <div className="end-date">
+                        <div className="end-title">
+                            End Date
+                        </div>
+                        <input type="text" placeholder="End Date"/>
+                    </div>
+                </div>
+                </>
+                }
             </div>
             <div className="experience">
                 <div className="title">
